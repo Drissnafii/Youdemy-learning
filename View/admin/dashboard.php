6d549f5e03afafce84db1ec23a3729349baa64f9
drissnafii <drissnafi3@gmail.com>
2025-01-16 14:06:52 +0100

    fix: add login methode with basic redirections

---
+++ View/admin/dashboard.php
@@ -0,0 +1,2 @@
+<?php
+echo "you are in the <h2>Admin Dashboard</h2>";
--- src/Models/User.php
+++ src/Models/User.php
@@ -1,26 +1,56 @@
 <?php
-require __DIR__ . '/../../config/database.php';
+// require __DIR__ . '/../../config/database.php';
 class User {
-    private int $userID;
-    private string $username;
-    private string $password;
-    private string $email;
-    private string $role;
-    private Database $db;
+    public $conn;
 
-    public function __construct(Database $db)
-    {
-        $this->db = $db;
+    public function register($username, $email, $password, $role) {
+        $conn = new PDO('mysql:host=localhost;dbname=youdemy_db;', 'root', '');
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO Users (username,email, password, role)
+        values (:username, :email, :password, :role)";
+        $stml = $conn->prepare($query);
+        $stml->execute([
+            "username" => $username,
+            "email" => $email,
+            "password" => $hashed_password,
+            "role" => $role,
+        ]);
+        $baseUrl = "http://" . 'localhost' . "/Youdemy-learning/View/courses/catalog.php";
+        header("location: $baseUrl");
     }
 
-    public function register(string $username, string $password,string $email, string $role = 'student') {
-        if (empty($username) || empty($email) || empty($password)) {
-            throw new Exception("Tous les champs sont obligatoire.");
+    public function login($email, $password) {
+        $conn = new PDO('mysql:host=localhost;dbname=youdemy_db;', 'root', '');
+        $query = "SELECT * FROM Users WHERE email = :email";
+        $stml = $conn->prepare($query);
+        $stml->execute([
+            "email" => $email
+        ]);
+        $user = $stml->fetch();
+        if ($user) {
+            if ($user["Email"] === "jesuisadmin@gmail.com") {
+                if (password_verify($password, $user['Password'])) {
+                    $adminDashPage = "http://" . 'localhost' . "/Youdemy-learning/View/admin/dashboard.php";
+                    header("location: $adminDashPage");
+                    exit();
+                }
+                echo "password of A, is incorrect";
+            }
+            if ($user["Email"] === "jesuisuser@gmail.com") {
+                if (password_verify($password, $user['Password'])) {
+                    $adminDashPage = "http://" . 'localhost' . "/Youdemy-learning/View/admin/dashboard.php";
+                    header("location: $adminDashPage");
+                    exit();
+                }
+                echo "password of U, is incorrect";
+            }
+            if (password_verify($password, $user['Password'])) {
+                $catalogPage = "http://" . 'localhost' . "/Youdemy-learning/View/courses/catalog.php";
+                header("location: $catalogPage");
+            }
         }
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            throw new Exception("Forma d'email invalide.");
-        }
-
-        
+        echo "Les donne est incorrect";
     }
 }
+
+// $driss = new User();
