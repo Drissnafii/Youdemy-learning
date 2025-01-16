403119d272f919a1b9db4ab01829de8392f530c4
drissnafii <drissnafi3@gmail.com>
2025-01-16 15:19:55 +0100

    fix: fix conflit with the another brach

--- src/Models/User.php
+++ src/Models/User.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-include_once __DIR__ . '/../../config/database.php';
-=======
 // require __DIR__ . '/../../config/database.php';
->>>>>>> feature/user-model
 class User {
     public $conn;
 
@@ -25,22 +21,32 @@ class User {
 
     public function login($email, $password) {
         $conn = new PDO('mysql:host=localhost;dbname=youdemy_db;', 'root', '');
-<<<<<<< HEAD
-        $query = "SELECT * FROM Users
-        WHERE email = :email";
-=======
         $query = "SELECT * FROM Users WHERE email = :email";
->>>>>>> feature/user-model
         $stml = $conn->prepare($query);
         $stml->execute([
             "email" => $email
         ]);
         $user = $stml->fetch();
         if ($user) {
-<<<<<<< HEAD
+            if ($user["Email"] === "jesuisadmin@gmail.com") {
+                if (password_verify($password, $user['Password'])) {
+                    $adminDashPage = "http://" . 'localhost' . "/Youdemy-learning/View/admin/dashboard.php";
+                    header("location: $adminDashPage");
+                    exit();
+                }
+                echo "password of A, is incorrect";
+            }
+            if ($user["Email"] === "jesuisuser@gmail.com") {
+                if (password_verify($password, $user['Password'])) {
+                    $adminDashPage = "http://" . 'localhost' . "/Youdemy-learning/View/admin/dashboard.php";
+                    header("location: $adminDashPage");
+                    exit();
+                }
+                echo "password of U, is incorrect";
+            }
             if (password_verify($password, $user['Password'])) {
-                $baseUrl = "http://" . 'localhost' . "/Youdemy-learning/View/courses/catalog.php";
-                header("location: $baseUrl");
+                $catalogPage = "http://" . 'localhost' . "/Youdemy-learning/View/courses/catalog.php";
+                header("location: $catalogPage");
             }
         }
         echo "Les donne est incorrect";
